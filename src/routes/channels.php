b309c6c09c6adb9ba563aff05de56fd9f76e9108
L-Eli <l.lin860218@gmail.com>
2018-09-27 15:45:47 +0800

    Revert "pusher"
    This reverts commit 1bb46c5873e5dea07f67bb6bb4e07346fbfd19e7.

--- src/routes/channels.php
+++ src/routes/channels.php
@@ -1,5 +1,5 @@
 <?php
- 
+
 /*
 |--------------------------------------------------------------------------
 | Broadcast Channels
@@ -10,11 +10,7 @@
 | used to check if an authenticated user can listen to the channel.
 |
 */
- 
+
 Broadcast::channel('App.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
-});
- 
-Broadcast::channel('user.{toUserId}', function ($user, $toUserId) {
-    return $user->id == $toUserId;
 });
